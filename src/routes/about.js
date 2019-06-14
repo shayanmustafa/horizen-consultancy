5e195da20179fe04ff5792bc42dcb29798b33a97
Shayan Mustafa <mustafa.shayan9@gmail.com>
2019-06-14 21:36:49 +0500

    setup about component

--- src/routes/about.js
+++ src/routes/about.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 import NavBar from '../components/navbar'
+import Footer from '../components/footer'
 import '../styles/about.css'
 
 class About extends Component {
     render() {
         return (
             <div className="about-container">
-                <NavBar />
-                <h1 className="title">About Route</h1>
+                <header className="page-header">
+                    <NavBar />
+                </header>
+                <main className="page-main">
+                    <h1 className="title">About Route</h1>
+                </main>
+                <footer className="page-footer">
+                    <Footer />
+                </footer>
             </div>
         )
     }
--- src/routes/home.js
+++ src/routes/home.js
@@ -13,29 +13,29 @@ class Home extends Component {
                     <NavBar />
                 </header>
                 <main className="page-main">
-                <div>
-                    <div class="text-wrapper title-wrapper">
-                        <h1><SplitText initialPose="exit" pose="enter" 
-                        charPoses={{exit: { opacity: 0 },
-                        enter: { opacity: 1, delay: ({ charIndex }) => charIndex * 50 }}}>
-                        Get your ERP solutions
-                    </SplitText></h1>
-                    </div>
-                    <div class="text-wrapper p-wrapper">
+                    <div>
+                        <div class="text-wrapper title-wrapper">
+                            <h1><SplitText initialPose="exit" pose="enter" 
+                            charPoses={{exit: { opacity: 0 },
+                            enter: { opacity: 1, delay: ({ charIndex }) => charIndex * 50 }}}>
+                            Get your ERP solutions
+                        </SplitText></h1>
+                        </div>
+                        <div class="text-wrapper p-wrapper">
+                            <p><SplitText initialPose="exit" pose="enter" 
+                            charPoses={{exit: { opacity: 0 },
+                            enter: { opacity: 1, delay: ({ charIndex }) => charIndex * 10 }}}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </SplitText></p>
+                        </div>
+                        <div class="text-wrapper p-wrapper">
                         <p><SplitText initialPose="exit" pose="enter" 
-                        charPoses={{exit: { opacity: 0 },
-                        enter: { opacity: 1, delay: ({ charIndex }) => charIndex * 10 }}}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </SplitText></p>
-                    </div>
-                    <div class="text-wrapper p-wrapper">
-                    <p><SplitText initialPose="exit" pose="enter" 
-                        charPoses={{exit: { opacity: 0 },
-                        enter: { opacity: 1, delay: ({ charIndex }) => charIndex * 10 }}}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    </SplitText></p>
-                    </div>
-                    </div>
+                            charPoses={{exit: { opacity: 0 },
+                            enter: { opacity: 1, delay: ({ charIndex }) => charIndex * 10 }}}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                        </SplitText></p>
+                        </div>
+                        </div>
                 </main>
                 <footer className="page-footer">
                     <Footer />

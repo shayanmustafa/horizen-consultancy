ba8e83da2468ac9767c6a760ff64a8085b76af73
Shayan Mustafa <mustafa.shayan9@gmail.com>
2019-06-14 14:34:32 +0500

    footer component created

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return(
+            <footer>
+                <small>
+                    © Horizen Consultancy Limited
+                </small>
+            </footer>
+        )
+    }
+}

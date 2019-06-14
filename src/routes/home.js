8d1286efcbac00233f1fc88fad18b93c17578ded
Shayan Mustafa <mustafa.shayan9@gmail.com>
2019-06-14 19:35:20 +0500

    text animation added

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import NavBar from '../components/navbar'
 import Footer from '../components/footer'
 import '../styles/home.css'
+import posed from 'react-pose';
+import SplitText from 'react-pose-text';
 
 class Home extends Component {
     render() {
@@ -12,14 +14,26 @@ class Home extends Component {
                 </header>
                 <main className="page-main">
                 <div>
-                    <div class="text-wrapper">
-                        <h1>Get your ERP solutions</h1>   
+                    <div class="text-wrapper title-wrapper">
+                        <h1><SplitText initialPose="exit" pose="enter" 
+                        charPoses={{exit: { opacity: 0 },
+                        enter: { opacity: 1, delay: ({ charIndex }) => charIndex * 50 }}}>
+                        Get your ERP solutions
+                    </SplitText></h1>
                     </div>
                     <div class="text-wrapper p-wrapper">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> 
+                        <p><SplitText initialPose="exit" pose="enter" 
+                        charPoses={{exit: { opacity: 0 },
+                        enter: { opacity: 1, delay: ({ charIndex }) => charIndex * 10 }}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </SplitText></p>
                     </div>
                     <div class="text-wrapper p-wrapper">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+                    <p><SplitText initialPose="exit" pose="enter" 
+                        charPoses={{exit: { opacity: 0 },
+                        enter: { opacity: 1, delay: ({ charIndex }) => charIndex * 10 }}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    </SplitText></p>
                     </div>
                     </div>
                 </main>

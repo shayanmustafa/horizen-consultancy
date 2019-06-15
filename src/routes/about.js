64857b42be7ccea622a1eb9a37983e6d43fde5fc
Shayan Mustafa <mustafa.shayan9@gmail.com>
2019-06-15 14:56:48 +0500

    about page: our team three columns

--- src/routes/about.js
+++ src/routes/about.js
@@ -28,10 +28,26 @@ class About extends Component {
                     <div className="section-3">
                         <h2>Our Team</h2>
                     </div>
+                    <br/>
                     <div className="section-4">
-                        <div className="section-4-1"></div>
-                        <div className="section-4-2"></div>
-                        <div className="section-4-3"></div>
+                        <div className="section-4-1">
+                            <h5>Person X</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                        <div className="section-4-2">
+                            <h5>Person Y</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                        <div className="section-4-3">
+                            <h5>Person Z</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
                     </div>
                 </main>
                 <footer className="about-footer">
